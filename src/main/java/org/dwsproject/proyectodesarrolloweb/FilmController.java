26feeabc0c873b980b2e51563d2289a93bb62320
aberdon2022 <alejandrob25082003@gmail.com>
2024-03-08 09:32:57 +0100

    Film Service

--- src/main/java/org/dwsproject/proyectodesarrolloweb/FilmController.java
+++ src/main/java/org/dwsproject/proyectodesarrolloweb/FilmController.java
@@ -1,4 +1,6 @@
 package org.dwsproject.proyectodesarrolloweb;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -8,39 +10,17 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import org.dwsproject.proyectodesarrolloweb.service.FilmService;
 
 @Controller
 public class FilmController {
-    // List of films
-    private ArrayList<Pelicula> pendingFilms = new ArrayList<>();
-    private ArrayList<Pelicula> completedFilms = new ArrayList<>();
+    @Autowired
+    private FilmService filmService;
 
-     // Method to obtain pending films
-     public List<Pelicula> getPendingFilms() {
-        return pendingFilms;
-    }
-
-    // Method to obtain completed films
-    public List<Pelicula> getCompletedFilms() {
-        return completedFilms;
-    }
-    
     @PostMapping("/addpeli")
     public String createFilm(Pelicula film, @RequestParam("image")MultipartFile imageFile, @RequestParam("listType") String listType) {
         try {
-            String folder = "src/main/resources/static/images/";
-            byte[] bytes = imageFile.getBytes();
-            Path path = Paths.get(folder + imageFile.getOriginalFilename());
-            Files.write(path, bytes);
-
-            film.setImagePath(imageFile.getOriginalFilename());//Save the image path
-            if ("pending".equals(listType)) {
-                pendingFilms.add(film);
-            } else if ("completed".equals(listType)) {
-                completedFilms.add(film);
-            }
+            filmService.addFilm(film, imageFile, listType);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -57,31 +37,31 @@ public class FilmController {
 
     @GetMapping("/AddPending")
     public String pending(Model model) {
-        model.addAttribute("films", pendingFilms);
+        model.addAttribute("films", filmService.getPendingFilms());
         return "AddPendingList";
     }
-    
+
     @GetMapping("/AddCompleted")
-        public String completed(Model model) {
-            model.addAttribute("films", completedFilms);
-            return "AddCompletedList";
+    public String completed(Model model) {
+        model.addAttribute("films", filmService.getCompletedFilms());
+        return "AddCompletedList";
     }
 
     @GetMapping("/ViewPending")
     public String viewPending(Model model) {
-        model.addAttribute("films", pendingFilms);
+        model.addAttribute("films", filmService.getPendingFilms());
         return "ViewPendingList";
+    }
+
+    @GetMapping("/ViewCompleted")
+    public String viewCompleted(Model model) {
+        model.addAttribute("films", filmService.getCompletedFilms());
+        return "ViewCompletedList";
     }
 
     @GetMapping("/ConfirmPending")
     public String confirmPending() {
         return "MessageAfterAddPending";
-    }
-
-    @GetMapping("/ViewCompleted")
-    public String viewCompleted(Model model) {
-        model.addAttribute("films", completedFilms);
-        return "ViewCompletedList";
     }
 
     @GetMapping("/ConfirmCompleted")
@@ -91,20 +71,15 @@ public class FilmController {
 
     @GetMapping("/ViewCompleted/{title}/delete")//Delete a film from the completed list
     public String deleteFilmC(Model model, @PathVariable String title) {
-        completedFilms.removeIf(p -> p.getTitle().equals(title));
+        filmService.deleteFilm(title, "completed");
         return "deletedCompletedFilm";
     }
     @GetMapping("/ViewPending/{title}/delete")//Delete a film from the pending list
     public String deleteFilmP(Model model, @PathVariable String title) {
-        pendingFilms.removeIf(p -> p.getTitle().equals(title));
+        filmService.deleteFilm(title, "pending");
         return "deletedPendingFilm";
     }
-
-
-
-}    
-    
-
+}
 
    
 
