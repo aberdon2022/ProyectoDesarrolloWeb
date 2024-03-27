59244d50846db9193d5edee49e6d3e6b70a11675
aberdon2022 <alejandrob25082003@gmail.com>
2024-03-27 10:28:03 +0100

    ProyectoDesarrolloWeb - DB - FaseAmigos - FasePost - API - ImageFinished - QuillJS - Dynamic Queries - Encapsulacion UserService

--- src/main/java/org/dwsproject/proyectodesarrolloweb/Controllers/UserController.java
+++ src/main/java/org/dwsproject/proyectodesarrolloweb/Controllers/UserController.java
@@ -3,6 +3,7 @@ package org.dwsproject.proyectodesarrolloweb.Controllers;
 import org.dwsproject.proyectodesarrolloweb.Classes.User;
 import org.dwsproject.proyectodesarrolloweb.service.UserService;
 import org.dwsproject.proyectodesarrolloweb.service.UserSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,15 +12,11 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class UserController {
 
+    @Autowired
+    private UserService userService;//uses methods of the UserService class
 
-    private final UserService userService;//uses methods of the UserService class
-
-    private final UserSession userSession;//uses methods of the UserSession class
-
-    public UserController(UserService userService, UserSession userSession) {
-        this.userService = userService;
-        this.userSession = userSession;
-    }
+    @Autowired
+    private UserSession userSession;//uses methods of the UserSession class
 
     @GetMapping("/login")  
     public String login() {
