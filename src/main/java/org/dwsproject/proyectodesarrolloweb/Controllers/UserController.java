0c4fe3814de371a17cf7307d98af34ce7f4d89a8
aberdon2022 <alejandrob25082003@gmail.com>
2024-04-10 15:42:18 +0200

    Ajustes finales

--- src/main/java/org/dwsproject/proyectodesarrolloweb/Controllers/UserController.java
+++ src/main/java/org/dwsproject/proyectodesarrolloweb/Controllers/UserController.java
@@ -1,4 +1,5 @@
 package org.dwsproject.proyectodesarrolloweb.Controllers;
+
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import org.dwsproject.proyectodesarrolloweb.Classes.User;
@@ -7,10 +8,11 @@ import org.dwsproject.proyectodesarrolloweb.Service.UserService;
 import org.dwsproject.proyectodesarrolloweb.Service.UserSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.net.Authenticator;
 
 @Controller
 public class UserController {
